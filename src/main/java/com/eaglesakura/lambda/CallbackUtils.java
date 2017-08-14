896e6f668d3286a0665ea514cf99ffbdac1f3090
eaglesakura <eagle.sakura@gmail.com>
2017-08-14 23:55:38 +0900

    指定時間キャンセルチェックに対応したsleepを追加

--- src/main/java/com/eaglesakura/lambda/CallbackUtils.java
+++ src/main/java/com/eaglesakura/lambda/CallbackUtils.java
@@ -1,6 +1,32 @@
 package com.eaglesakura.lambda;
 
+import com.eaglesakura.util.Timer;
+
 public class CallbackUtils {
+
+    /**
+     * 指定時間だけウェイトをかける。
+     * <p>
+     * 途中でキャンセルされた場合は例外を投げて終了される
+     */
+    public void waitTime(long timeMs, CancelCallback cancelCallback) throws InterruptedException {
+        if (timeMs <= 0) {
+            return;
+        }
+
+        Timer timer = new Timer();
+        while (timer.end() < timeMs) {
+            try {
+                Thread.sleep(1);
+            } catch (Exception e) {
+            }
+
+            if (isCanceled(cancelCallback)) {
+                throw new InterruptedException();
+            }
+        }
+    }
+
     public static boolean isCanceled(CancelCallback callback) {
         if (callback == null) {
             return false;
